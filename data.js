c05ff34b6bfc3a46bc9146c69968645b70f1764a
Luke Miller <lukem1293@gmail.com>
2020-03-23 10:20:31 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,304 @@
+const data = [
+  {
+    id: 0,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 2500,
+        serial: "ser001",
+        sidingType: "wood",
+        size: "8x12",
+        sizeAsNum: 0812,
+        style: "utilityCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Home Office",
+        },
+      },
+    },
+  },
+  {
+    id: 1,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 2795,
+        serial: "ser002",
+        sidingType: "metal",
+        size: "10x16",
+        sizeAsNum: 1016,
+        style: "highwallBarn",
+      },
+      shedStatus: "Used",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Tullahoma, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 2,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 2450,
+        serial: "ser003",
+        sidingType: "vinyl",
+        size: "10x12",
+        sizeAsNum: 1012,
+        style: "utilityCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Home Office",
+        },
+      },
+    },
+  },
+  {
+    id: 3,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 3795,
+        serial: "ser004",
+        sidingType: "wood",
+        size: "10x20",
+        sizeAsNum: 1020,
+        style: "utilityCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "McMinnville, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 4,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 4795,
+        serial: "ser005",
+        sidingType: "vinyl",
+        size: "10x20",
+        sizeAsNum: 1020,
+        style: "utilityCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Murfreesboro, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 5,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      saleItem: "Yes",
+      shedInfo: {
+        price: 7135,
+        serial: "ser006",
+        sidingType: "metal",
+        size: "12x28",
+        sizeAsNum: 1228,
+        style: "highwallBarn",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Murfreesboro, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 6,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 1450,
+        serial: "ser007",
+        sidingType: "wood",
+        size: "8x12",
+        sizeAsNum: 0812,
+        style: "economyCabin",
+      },
+      shedStatus: "Used",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Home Office",
+        },
+      },
+    },
+  },
+  {
+    id: 7,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 3340,
+        serial: "ser008",
+        sidingType: "metal",
+        size: "10x20",
+        sizeAsNum: 1020,
+        style: "utilityCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Tullahoma, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 8,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 3650,
+        serial: "ser009",
+        sidingType: "metal",
+        size: "12x24",
+        sizeAsNum: 1224,
+        style: "classicDutch",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "McMinnville, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 9,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 2500,
+        serial: "ser010",
+        sidingType: "metal",
+        size: "10x16",
+        sizeAsNum: 1016,
+        style: "utilityCabin",
+      },
+      shedStatus: "Used",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Murfreesboro, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 10,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      saleItem: "Yes",
+      shedInfo: {
+        price: 4095,
+        serial: "ser011",
+        sidingType: "wood",
+        size: "12x20",
+        sizeAsNum: 1220,
+        style: "highwallBarn",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Tullahoma, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 11,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 7905,
+        serial: "ser012",
+        sidingType: "vinyl",
+        size: "12x24",
+        sizeAsNum: 1224,
+        style: "utilityCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Home Office",
+        },
+      },
+    },
+  },
+  {
+    id: 12,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 4400,
+        serial: "ser013",
+        sidingType: "metal",
+        size: "12x20",
+        sizeAsNum: 1220,
+        style: "highwallBarn",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "McMinnville, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 13,
+    inventoryItemDetails: {
+      saleItem: "Yes",
+      shedInfo: {
+        price: 1150,
+        serial: "ser014",
+        sidingType: "wood",
+        size: "8x12",
+        sizeAsNum: 0812,
+        style: "economyCabin",
+      },
+      shedStatus: "New",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "Murfreesboro, TN",
+        },
+      },
+    },
+  },
+  {
+    id: 14,
+    inventoryItemDetails: {
+      __typename: "inventoryItem",
+      shedInfo: {
+        price: 4095,
+        serial: "ser015",
+        sidingType: "metal",
+        size: "12x20",
+        sizeAsNum: 1220,
+        style: "utilityCabin",
+      },
+      shedStatus: "Used",
+      shedLocation: {
+        salesLotInfo: {
+          pageTitle: "McMinnville, TN",
+        },
+      },
+    },
+  },
+]
